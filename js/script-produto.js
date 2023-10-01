0ffcc1596fd96976426889bd21ae3e01906c5b14
Ana Paula Santos <santosilva.anapaula@gmail.com>
2023-10-01 16:57:52 -0300

    Inclusão do Script da pagina de produto

---
+++ js/script-produto.js
@@ -0,0 +1,31 @@
+let coracao = document.querySelector(".bi-heart-fill");
+coracao.addEventListener("click", function(event) {
+    if(event.target.nodeName === "I"){
+        event.target.classList.toggle("red")
+    }
+})
+
+//Incrementar e decrementar quantidade de produtos
+let decrementar = document.querySelector(".bi-dash-square");
+let incrementar = document.querySelector(".bi-plus-square");
+
+let textoProduto = document.querySelector(".produto-numero-quantidade");
+let quantidadeProduto = parseInt(textoProduto.textContent);
+
+incrementar.addEventListener("click", function(){
+    quantidadeProduto++;
+    textoProduto.innerHTML = quantidadeProduto;
+});
+
+decrementar.addEventListener("click", function(){
+    if(quantidadeProduto <= 1){
+        alert("Aquantidade de produto não pode ser menor que 1!")
+    }else{
+        quantidadeProduto--;
+        textoProduto.innerHTML = quantidadeProduto;
+    }
+});
+
+console.log(decrementar);
+console.log(incrementar);
+console.log(quantidadeProduto);
